ff0ff69bcf4eb6a9a5b3cea057176e45114799ca
David <david.cauner@gmail.com>
2024-12-08 19:27:58 +0100

    Added nfc_io

---
+++ Core/Inc/nfc_io.h
@@ -0,0 +1,26 @@
+/*
+ * nfc_io.h
+ *
+ *  Created on: Dec 8, 2024
+ *      Author: david
+ */
+
+#ifndef INC_NFC_IO_H_
+#define INC_NFC_IO_H_
+
+
+#include "stdint.h"
+#include "stm32f3xx_ll_i2c.h"
+#include "stm32f3xx_ll_gpio.h"
+
+void NFC_IO_Init(uint8_t GpoIrqEnable);
+void NFC_IO_DeInit(void);
+uint16_t NFC_IO_ReadMultiple(uint8_t Addr, uint8_t *pBuffer, uint8_t Length);
+uint16_t NFC_IO_WriteMultiple(uint8_t Addr, uint8_t *pBuffer, uint8_t Length);
+uint16_t NFC_IO_IsDeviceReady(uint8_t Addr, uint32_t Trials);
+void NFC_IO_ReadState(uint8_t *pPinState);
+void NFC_IO_RfDisable(uint8_t PinState);
+void NFC_IO_Delay(uint32_t Delay);
+
+
+#endif /* INC_NFC_IO_H_ */
---
+++ Core/Src/nfc_io.c
@@ -0,0 +1,34 @@
+/*
+ * nfc_io.c
+ *
+ *  Created on: Dec 8, 2024
+ *      Author: david
+ */
+
+
+#include "nfc_io.h"
+
+void NFC_IO_Init(uint8_t GpoIrqEnable){
+
+}
+void NFC_IO_DeInit(void){
+
+}
+uint16_t NFC_IO_ReadMultiple(uint8_t Addr, uint8_t *pBuffer, uint8_t Length){
+
+}
+uint16_t NFC_IO_WriteMultiple(uint8_t Addr, uint8_t *pBuffer, uint8_t Length){
+
+}
+uint16_t NFC_IO_IsDeviceReady(uint8_t Addr, uint32_t Trials){
+
+}
+void NFC_IO_ReadState(uint8_t *pPinState){
+
+}
+void NFC_IO_RfDisable(uint8_t PinState){
+
+}
+void NFC_IO_Delay(uint32_t Delay){
+
+}
